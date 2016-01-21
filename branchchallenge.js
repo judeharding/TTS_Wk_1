a2c718dd52227d60da613fbd88f810921bbd12e9
Jude Harding <jharding@genesisgroupusa.com>
2016-01-21 17:14:32 -0500

    Re Initializing

---
+++ branchchallenge.js
@@ -0,0 +1,14 @@
+
+var temp = 90;
+
+var precip = "raining";
+
+var indoors = true;
+
+console.log ("The temperature is " + temp + " degrees");
+
+if (temp > 80 && precip === false){
+  console.log ("Time to swim");
+} else if (indoors){
+  console.log("Time to swim!");
+}
